79f0ecfc0f019c945a9f973bb0c992ce2fee59d4
sitegovdados-rgb <site.govdados@gmail.com>
2026-05-06 11:35:41 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-const SENHA_CORRETA = process.env.SITE_SENHA || 'cidadeintegrada@secc'
+const SENHA_CORRETA = process.env.SITE_SENHA
 const COOKIE_NAME   = 'govdados_auth'
 const COOKIE_VALOR  = 'autorizado'
 
